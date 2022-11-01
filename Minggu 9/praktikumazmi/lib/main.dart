715dea7e2078c78a04664f821d38c8c3b1a1eda1
2azmi2 <azmikamikaze@gmail.com>
2022-11-01 11:15:39 +0700

    done

--- Minggu 9/praktikumazmi/lib/main.dart
+++ Minggu 9/praktikumazmi/lib/main.dart
@@ -35,16 +35,152 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'R.Muhammad Azmi Herdi Shofiyullah',
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+              color: Color.fromRGBO(205, 240, 255, 0.7),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Text(
+                    "NEWS",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "HOT NEWS",
+                    style: TextStyle(fontSize: 25),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(10, 0, 10, 20),
+              decoration: BoxDecoration(
+                  border: Border.all(width: 1.0, color: Colors.blueAccent)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    height: 200,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        alignment: Alignment.center,
+                        fit: BoxFit.fitWidth,
+                        image: AssetImage('assets/images/gambar1.jpg'),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                    color: Color.fromRGBO(205, 240, 255, 0.8),
+                    child: const Text(
+                      "Sea of stars in indonesia night sky",
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    color: Color.fromRGBO(145, 224, 255, 0.8),
+                    padding: const EdgeInsets.fromLTRB(10, 15, 10, 15),
+                    child: const Text(
+                      "NIGHT SKY",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+              decoration: BoxDecoration(
+                border: Border.all(width: 1, color: Colors.lightBlue),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Image.asset("assets/images/gambar2.jpg"),
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.fromLTRB(16, 0, 0, 16),
+                          decoration: const BoxDecoration(
+                              color: Color.fromRGBO(205, 240, 255, 0.7)),
+                          child: const Text(
+                              "Japan people create a image of stars rain"),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(10),
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        top: BorderSide(width: 1, color: Colors.blueAccent),
+                      ),
+                      color: Colors.blueAccent,
+                    ),
+                    child: const Text("Malang, 01 November 2022"),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+              decoration: BoxDecoration(
+                border: Border.all(width: 1, color: Colors.blueAccent),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Image.asset("assets/images/gambar3.jpg"),
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.fromLTRB(16, 0, 0, 16),
+                          decoration: const BoxDecoration(
+                              color: Color.fromRGBO(205, 240, 255, 0.7)),
+                          child: const Text(
+                              "There's stars fall in Malang night sky"),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(10),
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        top: BorderSide(width: 1, color: Colors.blueAccent),
+                      ),
+                      color: Colors.blueAccent,
+                    ),
+                    child: const Text("Malang, 01 November 2022"),
+                  )
+                ],
+              ),
             ),
           ],
         ),
-      ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
