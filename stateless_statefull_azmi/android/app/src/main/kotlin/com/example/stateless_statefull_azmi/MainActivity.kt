ef1db840f8c6ce175519d260a06cf0f92cdb52fc
2azmi2 <azmikamikaze@gmail.com>
2022-11-08 10:34:10 +0700

    praktikum11

---
+++ stateless_statefull_azmi/android/app/src/main/kotlin/com/example/stateless_statefull_azmi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stateless_statefull_azmi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ stateless_statefull_azmi/lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Konverter Suhu"),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(8),
+          child: Column(
+            children: [
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  hintText: 'Masukkan Suhu Dalam Celcius',
+                ),
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly,
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 200),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(right: 10),
+                          child: Text(
+                            "Suhu Dalam Kelvin",
+                            style: TextStyle(fontSize: 20),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Container(
+                          child: Text("298", style: TextStyle(fontSize: 20)),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(left: 10),
+                          child: Text("Suhu dalam Reamur",
+                              style: TextStyle(fontSize: 20)),
+                        ),
+                        Container(
+                          child: Text(
+                            "20",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 230),
+                width: 400,
+                child: TextButton(
+                  style:
+                      TextButton.styleFrom(backgroundColor: Colors.blueAccent),
+                  child: Text(
+                    "Konversi Suhu",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
